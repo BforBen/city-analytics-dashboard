0a153beab5cfd26d6dde95e713f38123d41fc83d
Ben <BforBen@users.noreply.github.com>
2016-10-28 12:20:34 +0100

    Change to global replace

--- server.rb
+++ server.rb
@@ -19,19 +19,19 @@ end
 
 get '/' do
   html = File.read(File.join('public', 'index.html'))
-  html.sub!('$PROFILE_ID', JSON.dump(ENV['GA_VIEW_ID']))
-  html.sub!('$DOMAIN_URL', JSON.dump(ENV['GA_WEBSITE_URL']))
-  html.sub!('$DOMAIN_URL_RAW', ENV['GA_WEBSITE_URL'])
-  html.sub!('$THEME_COLOUR', ENV['THEME_COLOUR'])
-  html.sub!('$ORGANISATION_NAME', ENV['ORGANISATION_NAME'])
-  html.sub!('$APP_TITLE', ENV['APP_TITLE'])
-  html.sub!('$SHORTCUT_ICON', ENV['SHORTCUT_ICON'])
+  html.gsub!('$PROFILE_ID', JSON.dump(ENV['GA_VIEW_ID']))
+  html.gsub!('$DOMAIN_URL', JSON.dump(ENV['GA_WEBSITE_URL']))
+  html.gsub!('$DOMAIN_URL_RAW', ENV['GA_WEBSITE_URL'])
+  html.gsub!('$THEME_COLOUR', ENV['THEME_COLOUR'])
+  html.gsub!('$ORGANISATION_NAME', ENV['ORGANISATION_NAME'])
+  html.gsub!('$APP_TITLE', ENV['APP_TITLE'])
+  html.gsub!('$SHORTCUT_ICON', ENV['SHORTCUT_ICON'])
   return html
 end
 
 get '/assets/style.css' do
   css = File.read(File.join('assets', 'style.css'))
-  css.sub!('$THEME_COLOUR', ENV['THEME_COLOUR'])
+  css.gsub!('$THEME_COLOUR', ENV['THEME_COLOUR'])
   return css
 end
 
